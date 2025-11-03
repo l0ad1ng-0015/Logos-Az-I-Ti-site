bf5387941175d9a9c15a2036c5443ce405b79317
Viktor PC <popow_viktor@abv.bg>
2025-11-04 00:49:51 +0200

    Преправени и останалите иконки

--- src/routes/seminari/+page.svelte
+++ src/routes/seminari/+page.svelte
@@ -38,57 +38,74 @@
     </div>
     <div id="h-right">
       <svg
-        class="second-svg"
-        style="right: 0;  display: flex; justify-content: end"
-        version="1.1"
+        class="first-svg"
         xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="110.8419 122.2011 248.1 248.1"
         width="70px"
         height="70px"
-        viewBox="0,0,255.99055,255.99055"
       >
-        <g transform="translate(38.39858,38.39858) scale(0.7,0.7)"
-          ><g
-            fill="#e2f099"
-            fill-rule="nonzero"
-            stroke="none"
-            stroke-width="1"
-            stroke-linecap="butt"
-            stroke-linejoin="miter"
-            stroke-miterlimit="10"
-            stroke-dasharray=""
-            stroke-dashoffset="0"
-            font-family="none"
-            font-weight="none"
-            font-size="none"
-            text-anchor="none"
-            style="mix-blend-mode: normal"
-            ><path
-              d="M127.99528,310.84567c-100.98549,0 -182.8504,-81.86491 -182.8504,-182.8504v0c0,-100.98549 81.86491,-182.8504 182.8504,-182.8504v0c100.98549,0 182.8504,81.86491 182.8504,182.8504v0c0,100.98549 -81.86491,182.8504 -182.8504,182.8504z"
-              id="shape"
-            ></path></g
-          ><g
-            fill="#ffffff"
-            fill-rule="nonzero"
-            stroke="none"
-            stroke-width="1"
-            stroke-linecap="butt"
-            stroke-linejoin="miter"
-            stroke-miterlimit="10"
-            stroke-dasharray=""
-            stroke-dashoffset="0"
-            font-family="none"
-            font-weight="none"
-            font-size="none"
-            text-anchor="none"
-            style="mix-blend-mode: normal"
-            ><g transform="scale(5.12,5.12)"
-              ><path
-                d="M25,4.0625c-12.58594,0 -22.9375,8.86328 -22.9375,19.9375c0,6.42578 3.5625,12.09375 8.9375,15.71875c-0.00781,0.21484 0,0.54688 -0.28125,1.59375c-0.34766,1.29297 -1.03516,3.125 -2.46875,5.15625l-1.03125,1.4375l1.78125,0.03125c6.17578,0.02734 9.75391,-4.03125 10.3125,-4.6875c1.82422,0.40625 3.72266,0.6875 5.6875,0.6875c12.58203,0 22.9375,-8.86328 22.9375,-19.9375c0,-11.07422 -10.35547,-19.9375 -22.9375,-19.9375zM25,5.9375c11.71484,0 21.0625,8.15234 21.0625,18.0625c0,9.91016 -9.34766,18.0625 -21.0625,18.0625c-2.00391,0 -3.94922,-0.24219 -5.78125,-0.6875l-0.5625,-0.125l-0.375,0.46875c0,0 -2.89062,3.25781 -7.5,4.03125c0.83203,-1.49219 1.46484,-2.87891 1.75,-3.9375c0.39844,-1.48047 0.40625,-2.5 0.40625,-2.5v-0.5l-0.4375,-0.28125c-5.22656,-3.3125 -8.5625,-8.58984 -8.5625,-14.53125c0,-9.91016 9.34375,-18.0625 21.0625,-18.0625z"
-              ></path></g
-            ></g
-          ></g
+        <g
+          fill="none"
+          fill-rule="nonzero"
+          stroke="none"
+          stroke-width="1"
+          stroke-linecap="butt"
+          stroke-linejoin="miter"
+          stroke-miterlimit="10"
+          stroke-dasharray=""
+          stroke-dashoffset="0"
+          font-family="none"
+          font-weight="none"
+          font-size="none"
+          text-anchor="none"
+          style="mix-blend-mode: normal"
+          transform="matrix(1, 0, 0, 1, 106.89191866905466, 118.25107206295286)"
         >
+          <g transform="scale(0.5,0.5)">
+            <path
+              d="M256,7.9c-137,0 -248.1,111.1 -248.1,248.1c0,137 111.1,248.1 248.1,248.1c137,0 248.1,-111.1 248.1,-248.1c0,-137 -111.1,-248.1 -248.1,-248.1z"
+              fill="#feecd3"
+            />
+            <g>
+              <path
+                d="M323,88.1h-132.9c-4.9,0 -8.9,4 -8.9,8.9v132.9c0,4.9 4,8.9 8.9,8.9h132.9c4.9,0 8.9,-4 8.9,-8.9v-132.9c0,-4.9 -4,-8.9 -8.9,-8.9z"
+                fill="#ffac71"
+              />
+              <text
+                style="fill: rgb(241, 230, 230); font-family: Bahnschrift; font-size: 41px; white-space: pre;"
+                transform="matrix(3.565438, 0, 0, 3.140884, -423.405016, -705.154641)"
+                x="177.985"
+                y="290.296">A</text
+              >
+            </g>
+            <g>
+              <path
+                d="M243.6,245.5h-133c-4.9,0 -8.9,4 -8.9,8.9v132.9c0,4.9 4,8.9 8.9,8.9h132.9c4.9,0 8.9,-4 8.9,-8.9v-132.8c0.1,-5 -3.9,-9 -8.8,-9z"
+                fill="#eb9783"
+              />
+              <text
+                style="fill: rgb(241, 230, 230); font-family: Bahnschrift; font-size: 41px; white-space: pre;"
+                transform="matrix(3.62775, 0, 0, 3.157958, -515.098067, -551.446882)"
+                x="177.985"
+                y="290.296">Б</text
+              >
+            </g>
+            <g>
+              <g>
+                <path
+                  d="M402.4,245.5h-132.9c-4.9,0 -8.9,4 -8.9,8.9v132.9c0,4.9 4,8.9 8.9,8.9h132.9c4.9,0 8.9,-4 8.9,-8.9v-132.8c0,-5 -4,-9 -8.9,-9z"
+                  fill="#edc92f"
+                />
+                <text
+                  style="fill: rgb(241, 230, 230); font-family: Bahnschrift; font-size: 41px; white-space: pre;"
+                  transform="matrix(3.581682, 0, 0, 3.157822, -347.678975, -552.280256)"
+                  x="177.985"
+                  y="290.296">B</text
+                >
+              </g>
+            </g>
+          </g>
+        </g>
       </svg>
       <svg
         class="second-svg"
